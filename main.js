19e508da7bb22a579c0688494d02afd935f02ff0
Ryan Cornett <ryandcornett@gmail.com>
2024-11-05 15:56:27 -0500

    Add necessary pages, header, hero, footer for feedback

--- main.js
+++ main.js
@@ -10,3 +10,12 @@ function displayTopButton() {
 }
 document.addEventListener('scroll', displayTopButton);
 
+const searchButton = document.getElementById('search-button');
+const dialog = document.querySelector('.dialog-overview');
+const closeButton = dialog.querySelector('sl-button[slot="footer"]');
+
+searchButton.addEventListener('click', () => dialog.show());
+
+let currentYear = new Date().getFullYear();
+let credit = document.querySelector('.credit');
+credit.textContent = `Particular Baptist Publishing House 2023-${currentYear}.`
